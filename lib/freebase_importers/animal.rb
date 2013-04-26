70d8a2f09d9ebd914cddf17664dc4dc9f994d418
Sam Schenkman-Moore <samsm@samsm.com>
2013-04-26 17:16:30 -0400

    Removed excess comments.

--- lib/freebase_importers/animal.rb
+++ lib/freebase_importers/animal.rb
@@ -12,23 +12,3 @@ module FreebaseImporters
     images!
   end
 end
-# module FreebaseImporters
-#   class President < Base
-#     map :name
-#     map :id
-#
-#     add_to_mql :"/government/us_president/presidency_number", []
-#     add_method :presidency_numbers, -> { data["/government/us_president/presidency_number"] }
-#
-#     map :"/people/person/date_of_birth"
-#     add_method :date_of_birth, -> {
-#       date = data["/people/person/date_of_birth"]
-#       date ? Date.parse(date) : nil rescue date
-#     }
-#
-#     map :"/people/person/height_meters"
-#
-#     add_to_mql :type, "/government/us_president"
-#     images!
-#   end
-# end
